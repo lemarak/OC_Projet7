a3b177b8f47b35e937acce25daccc41c26131abf
lemarak <sdauguet@gmail.com>
2020-05-31 19:12:25 +0200

    Initialisation flask, bootstrap

---
+++ grandpyapp/views.py
@@ -0,0 +1,18 @@
+"""
+The views and routes of the flask application
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/')
+def index():
+    """ route for index """
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ run.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# coding: utf-8
+"""
+    Run app Flask
+"""
+
+from grandpyapp import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
