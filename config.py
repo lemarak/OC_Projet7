6904e21aaad397933e2bb9ac155c0da1ec816274
lemarak <sdauguet@gmail.com>
2020-06-11 16:59:48 +0200

    Heroku

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 import os
 
 URL_GOOGLE = "https://maps.googleapis.com/maps/api/geocode/json"
-KEY_GOOGLE = ""
+KEY_GOOGLE = os.environ['KEY_GOOGLE']
 
 URL_MEDIA_WIKI = "https://fr.wikipedia.org/w/api.php"
 
--- procfile
+++ procfile
@@ -1,1 +1,2 @@
 web: gunicorn grandpyapp:app
+init: FLASK_APP=run.py
